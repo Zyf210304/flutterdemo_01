ece691e2894ac8f53e02ac1efd6212d53fa93333
张亚飞 <672233580@qq.com>
2024-03-08 14:44:19 +0800

    Icon使用 main23 demoFont

---
+++ lib/demoFont.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DemoIconFont {
+  static const IconData book = IconData(
+    0xf00a1,
+    fontFamily: "DemoFont",
+    matchTextDirection: true,
+  );
+
+  static const IconData shuba = IconData(
+    0xe624,
+    fontFamily: "DemoFont",
+    matchTextDirection: true,
+  );
+
+  static const IconData tushuyinxiang = IconData(
+    0x3461,
+    fontFamily: "DemoFont",
+    matchTextDirection: true,
+  );
+}
